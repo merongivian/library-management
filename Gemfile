754f1ef078774f8aaa99b6aa17700d02b58e5ed3
Jose Añasco <joseanasco1@gmail.com>
2013-11-09 13:29:38 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+#html and stuff
+gem 'slim-rails', '~> 2.0.3'
+gem 'bootstrap-sass-rails', '~> 3.0.0.3'
+
+#schema
+gem 'foreigner'
+
+group :developmet, :test do
+  gem 'quiet_assets'
+  gem 'rspec-rails', '~> 2.0'
+end
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'foreigner-matcher'
+  gem 'shoulda-matchers'
+  gem 'cucumber-rails', '~> 1.4.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibraryManagement::Application.config.secret_key_base = '91157ec238b76aacfd43f8079023f990c9889c8db1eca0f4f5dde1ff9387e844e8140e20b8b8689d9d95e9d82b8be699cb840a6a243c9c59dd3f4f2f9ea01e10'
